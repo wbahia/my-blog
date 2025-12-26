20802272a2a8fcb8d84d82305e3b47f6d87d0ced
Walter Bahia <wbahia@gmail.com>
2025-12-26 17:16:44 -0300

    added images and post model

---
+++ src/models/post/post-model.ts
@@ -0,0 +1,13 @@
+export type PostModel =
+{
+  id: string;
+  title: string;
+  slug: string;
+  excerpt: string;
+  content: string;
+  coverImageUrl: string;
+  published: boolean;
+  createdAt: string;
+  updatedAt: string;
+  author: string;
+}
