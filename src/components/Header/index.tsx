e4e750e85b24be358bc7ae04b883104b6624a25c
Walter Bahia <wbahia@gmail.com>
2025-12-26 16:35:46 -0300

    changing folders

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+export function Header() {
+  return (
+    <header className="bg-gray-800 text-white p-4">
+      <h1 className="text-2xl font-bold">Meu Blog</h1>
+    </header>
+  );
+}
